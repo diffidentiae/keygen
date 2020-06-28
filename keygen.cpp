e8da7c1983d7a990858280646b8788fda828506d
Oleg Ershov <32275273+diffidentiae@users.noreply.github.com>
2020-06-28 05:11:51 +0300

    Add files via upload

---
+++ keygen.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+#include <set>
+#include <string> 
+#include <time.h>
+
+using namespace std;
+
+string generator(const string alphabet, int length) {
+    int pos;
+    string key;
+    set<int> posSet;
+    for (int i = 0; i < length; i++) {
+        pos = 0 + rand() % 16;
+        key += alphabet.at(pos);
+        posSet.insert(pos);
+    }
+
+    if (posSet.size() != 16) {
+        key = generator(alphabet, length);
+    }
+    return key;
+}
+
+int main() {
+    srand(time(NULL));
+    setlocale(LC_ALL, "Russian");
+    string alphabet = "0123456789abcdef";
+    string c;
+
+    cout << "Введите положение файла ключа: " << endl;
+    getline(cin, c);
+
+    int pos;
+    string key, iv;
+    set<int> posSet;
+    key = generator(alphabet, 64);
+    iv = generator(alphabet, 32);
+ 
+    ofstream file(c);
+    file << key << endl << iv;
+}
